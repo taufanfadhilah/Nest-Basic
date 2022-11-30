1207d5ce1d1c3be66d36560bd522843ed1d32aad
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2022-11-30 10:50:26 +0700

    setup jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { User } from './user/entity/user.entity';
 import { UserModule } from './user/user.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  controllers: [AppController, AuthController],
+  controllers: [AppController],
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
